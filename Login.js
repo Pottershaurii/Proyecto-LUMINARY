03c31e76cdeec31d693039b97dc9d0594587ff65
Pottershaurio <70236173+Pottershaurii@users.noreply.github.com>
2024-11-18 23:44:49 -0500

    Inicio de sesion obligatorio

--- Login.js
+++ Login.js
@@ -1,17 +1,16 @@
-const valUser = "admin"
-const valPassword = "123456789"
+const valUser = "admin";
+const valPassword = "123456789";
 
-function login(){
+function login(event) {
+    event.preventDefault();
 
-  const user = document.getElementById("user").value;
-  const password = document.getElementById("password").value;
+    const user = document.getElementById("user").value;
+    const password = document.getElementById("password").value;
 
-  if (user == valUser && password == valPassword){
-
-    alert("Inicio de sesion exitoso");
-    window.location.href = "index.html"
-
-  }else{
-    alert("Inicio de sesion incorrecto")
-  }
+    if (user === valUser && password === valPassword) {
+        alert("Inicio de sesión exitoso");
+        window.location.href = "secundario.html";
+    } else {
+        alert("Inicio de sesión incorrecto");
+    }
 }
